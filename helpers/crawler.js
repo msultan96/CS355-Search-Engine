14371fb59e64b173340d20d955c6a0fedefaa70d
Hammad Ahmad <hahmad0316@gmail.com>
2018-12-16 00:14:29 -0500

    push displayData function

--- helpers/crawler.js
+++ helpers/crawler.js
@@ -4,10 +4,11 @@ const mongoose = require('mongoose');
 require('../models/Search');
 const search = mongoose.model('Search');
 var temp = new Array();
+var display = require('./displayData');
 
 function queryCrawler(query){
     return new Promise((resolve, reject) => {
-        var maxDepth = 25;
+        var maxDepth = 100;
         var depth = 0;
         var c = new Crawler({
             maxConnections : 10,
@@ -19,7 +20,7 @@ function queryCrawler(query){
                     if(typeof $ !== 'function') return;
                     var page = result.body;
                     var lang = $(result.body).closest('[lang]').attr('lang') || 'en';
-                    console.log(lang);
+                    //console.log(lang);
                     if(lang.includes('en')){
                         var res = count(query, page);
                         if(res > 0){
@@ -51,12 +52,26 @@ function queryCrawler(query){
                       function(err, stu) {
                       // console.log("done");
                     });
+                    // depth = 0;
+                    //display gets called multiple times, need to find a way to call it once
+                    // console.log(temp);
+                    var arr = temp;
+                    arr.sort((a,b) => (a.relevancy > b.relevancy) ? -1 : ((b.relevancy > a.relevancy) ? 1 : 0));
+                    console.log("Iteration 1");
+                    display(arr);
+                    // display(temp.sort((a,b) => (a.relevancy > b.relevancy) ? -1 : ((b.relevancy > a.relevancy) ? 1 : 0)))
+                    //as soon as resolve gets called, it displays page
                     resolve();
                     return;
                 }
                 done();
             }
         });
+
+        // var arr = temp;
+        // arr.sort((a,b) => (a.relevancy > b.relevancy) ? -1 : ((b.relevancy > a.relevancy) ? 1 : 0));
+        // console.log("Iteration 1");
+        // display(arr);
 
         var link = `https://en.wikipedia.org/wiki/${encodeURIComponent(query)}`;
         console.log(`link = ${link}`)
